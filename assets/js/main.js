ac8b480130fa44adcd3343529f9dd98f24c66eab
yakupsevik <smyakupsevik@gmail.com>
2024-01-07 22:54:44 +0300

    update

---
+++ assets/js/main.js
@@ -0,0 +1,228 @@
+(function ($) {
+  "use strict";
+
+  var cfg = {
+      scrollDuration: 800, // smoothscroll duration
+    },
+    $WIN = $(window);
+
+  // Add the User Agent to the <html>
+  // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+  var doc = document.documentElement;
+  doc.setAttribute("data-useragent", navigator.userAgent);
+
+  // svg fallback
+  if (!Modernizr.svg) {
+    $(".header-logo img").attr("src", "images/logo.png");
+  }
+
+  /* Preloader
+   * -------------------------------------------------- */
+  var ssPreloader = function () {
+    $("html").addClass("ss-preload");
+
+    $WIN.on("load", function () {
+      //force page scroll position to top at page refresh
+      $("html, body").animate({ scrollTop: 0 }, "normal");
+
+      // will first fade out the loading animation
+      $("#loader").fadeOut("slow", function () {
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+      });
+
+      // for hero content animations
+      $("html").removeClass("ss-preload");
+      $("html").addClass("ss-loaded");
+    });
+  };
+
+  /* Menu on Scrolldown
+   * ------------------------------------------------------ */
+  var ssMenuOnScrolldown = function () {
+    var menuTrigger = $(".header-menu-toggle");
+
+    $WIN.on("scroll", function () {
+      if ($WIN.scrollTop() > 150) {
+        menuTrigger.addClass("opaque");
+      } else {
+        menuTrigger.removeClass("opaque");
+      }
+    });
+  };
+
+  /* OffCanvas Menu
+   * ------------------------------------------------------ */
+  var ssOffCanvas = function () {
+    var menuTrigger = $(".header-menu-toggle"),
+      nav = $(".header-nav"),
+      closeButton = nav.find(".header-nav__close"),
+      siteBody = $("body"),
+      mainContents = $("section, footer");
+
+    // open-close menu by clicking on the menu icon
+    menuTrigger.on("click", function (e) {
+      e.preventDefault();
+      siteBody.toggleClass("menu-is-open");
+    });
+
+    // close menu by clicking the close button
+    closeButton.on("click", function (e) {
+      e.preventDefault();
+      menuTrigger.trigger("click");
+    });
+
+    // close menu clicking outside the menu itself
+    siteBody.on("click", function (e) {
+      if (!$(e.target).is(".header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span")) {
+        siteBody.removeClass("menu-is-open");
+      }
+    });
+  };
+
+  /* Masonry
+   * ---------------------------------------------------- */
+  var ssMasonryFolio = function () {
+    var containerBricks = $(".masonry");
+
+    containerBricks.imagesLoaded(function () {
+      containerBricks.masonry({
+        itemSelector: ".masonry__brick",
+        resize: true,
+        columnWidth: 576,
+        columnHeight: 920,
+      });
+    });
+  };
+
+  /* photoswipe
+   * ----------------------------------------------------- */
+  var ssPhotoswipe = function () {
+    var items = [],
+      $pswp = $(".pswp")[0],
+      $folioItems = $(".item-folio");
+
+    // get items
+    $folioItems.each(function (i) {
+      var $folio = $(this),
+        $thumbLink = $folio.find(".thumb-link"),
+        $title = $folio.find(".item-folio__title"),
+        $caption = $folio.find(".item-folio__caption"),
+        $titleText = "<h4>" + $.trim($title.html()) + "</h4>",
+        $captionText = $.trim($caption.html()),
+        $href = $thumbLink.attr("href"),
+        $size = $thumbLink.data("size").split("x"),
+        $width = $size[0],
+        $height = $size[1];
+
+      var item = {
+        src: $href,
+        w: $width,
+        h: $height,
+      };
+
+      if ($caption.length > 0) {
+        item.title = $.trim($titleText + $captionText);
+      }
+
+      items.push(item);
+    });
+
+    // bind click event
+    $folioItems.each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        var options = {
+          index: i,
+          showHideOpacity: true,
+        };
+
+        // initialize PhotoSwipe
+        var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+        lightBox.init();
+      });
+    });
+  };
+
+  /* Smooth Scrolling
+   * ------------------------------------------------------ */
+  var ssSmoothScroll = function () {
+    $(".smoothscroll").on("click", function (e) {
+      var target = this.hash,
+        $target = $(target);
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $target.offset().top,
+          },
+          cfg.scrollDuration,
+          "swing"
+        )
+        .promise()
+        .done(function () {
+          // check if menu is open
+          if ($("body").hasClass("menu-is-open")) {
+            $(".header-menu-toggle").trigger("click");
+          }
+
+          window.location.hash = target;
+        });
+    });
+  };
+
+  /* Alert Boxes
+   * ------------------------------------------------------ */
+  var ssAlertBoxes = function () {
+    $(".alert-box").on("click", ".alert-box__close", function () {
+      $(this).parent().fadeOut(500);
+    });
+  };
+
+  /* Animate On Scroll
+   * ------------------------------------------------------ */
+  var ssAOS = function () {
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: "ease-in-sine",
+      delay: 300,
+      once: true,
+      disable: "mobile",
+    });
+  };
+
+  /* Initialize
+   * ------------------------------------------------------ */
+  (function clInit() {
+    ssPreloader();
+    ssMenuOnScrolldown();
+    ssOffCanvas();
+    ssMasonryFolio();
+    ssPhotoswipe();
+    ssSmoothScroll();
+    ssAlertBoxes();
+    ssAOS();
+    new Splide("#image-slider").mount();
+
+    const downloadButtons = document.querySelectorAll(".download-btn");
+
+    function downloadImage(event) {
+      const slide = event.target.closest(".splide__slide");
+      const image = slide.querySelector("img");
+      const imageSrc = image.getAttribute("src");
+      const link = document.createElement("a");
+      link.href = imageSrc;
+      link.download = "image.jpg";
+      link.click();
+    }
+
+    downloadButtons.forEach(function (button) {
+      button.addEventListener("click", downloadImage);
+    });
+  })();
+})(jQuery);
